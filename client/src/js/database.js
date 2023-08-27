ef91dd66c5becc5439dfe28aab9e115a0b8eed93
gsr142 <gsr142@gmail.com>
2023-08-27 16:04:16 -0700

    add get and post requests for indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,46 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  // Create connection to the database
+  const db = await openDB('jate', 1);
+  // Create new transaction and specify the db and readwrite privilege
+  const tx = db.transaction('jate', 'readwrite');
+  // Open up the desired object store
+  const store = tx.objectStore('jate');
+  // Use the .put() method to add content
+  const request = store.put({ id: 1, value: content });
+
+  try{
+    await request;
+    console.info("Content added to the database");
+  } catch(err){
+    console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    
+    const tx = db.transaction('jate', 'readonly');
+    
+    const store = tx.objectStore('jate');
+    
+    const request = store.getAll();
+    
+    const result = await request;
+    console.log('result.value', result);
+    
+    console.info("Retrieving information from the database");
+
+    return result;
+  } catch (err) {
+    console.error('getDb not implemented', err);
+    
+  }
+};
+
 
 initdb();
